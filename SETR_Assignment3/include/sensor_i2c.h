15425d7928de8ebba5f4ebbd17206596d01eb39a
duartecaldeira20 <duartecaldeira@ua.pt>
2025-06-02 23:49:59 +0100

    temperature ok

---
+++ SETR_Assignment3/include/sensor_i2c.h
@@ -0,0 +1,6 @@
+#ifndef SENSOR_I2C_H
+#define SENSOR_I2C_H
+
+int sensor_init_read(void);
+
+#endif
---
+++ SETR_Assignment3/src/sensor_i2c.c
@@ -0,0 +1,34 @@
+#include <zephyr/sys/printk.h>
+#include <zephyr/drivers/i2c.h>
+#include "sensor_i2c.h"
+
+#define ERR_FATAL -1
+#define TC74_CMD_RTR 0x00
+#define I2C0_NID DT_NODELABEL(tc74sensor)
+static const struct i2c_dt_spec dev_i2c = I2C_DT_SPEC_GET(I2C0_NID);
+
+int sensor_init_read(void)
+{
+    int ret = 0;
+    uint8_t temp = 0;
+    uint8_t cmd = TC74_CMD_RTR;
+
+    if (!device_is_ready(dev_i2c.bus)) {
+        printk("I2C bus %s is not ready!\n", dev_i2c.bus->name);
+        return ERR_FATAL;
+    }
+
+    ret = i2c_write_dt(&dev_i2c, &cmd, 1);
+    if (ret != 0) {
+        printk("Failed to write RTR command to I2C addr 0x%x, reg 0x%x\n", dev_i2c.addr, TC74_CMD_RTR);
+        return ERR_FATAL;
+    }
+
+    ret = i2c_read_dt(&dev_i2c, &temp, sizeof(temp));
+    if (ret != 0) {
+        printk("Failed to read from I2C addr 0x%x, reg 0x%x\n", dev_i2c.addr, TC74_CMD_RTR);
+        return ERR_FATAL;
+    }
+
+    return (int)temp;
+}
